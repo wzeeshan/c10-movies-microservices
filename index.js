3e546831ce9ea800551c9b5eee21eb69ad104a3e
wzeeshan <wajahatzeeshan@gmail.com>
2023-06-20 16:00:36 +0100

    added code

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', function (req, res){
+    res.send('hello lloyds')
+});
+
+app.get('/movies', function (req, res){
+    res.send('request made to movies endpoint')
+});
+
+app.listen(3000, function () {
+  console.log('App is listening on port 3000')
+})
